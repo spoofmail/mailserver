c264636379d42c4a6ee5ba82558af8b23daa5ba6
Vincent Costa <costa.vincent132@gmail.com>
2023-02-14 17:56:19 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const mailin = require('node-mailin')
 const axios = require('axios')
 
-const spoofMailHerokuServer = 'https://spoofmail-lambda.herokuapp.com/api/messages/'
+const spoofMailHerokuServer = 'https://spoofmail-us.herokuapp.com/api/messages/'
 
 mailin.start({
     port: 25,
-    webhook: 'https://spoofmail-lambda.herokuapp.com/api/messages/'
+    webhook: 'https://spoofmail-us.herokuapp.com/api/messages/'
 })
 
 mailin.on('message', function(connection, data, content) {
